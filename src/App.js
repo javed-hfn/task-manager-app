1eaf41042e418060d0810b270a5a6365701771e1
javed-hfn <javed.a@volunteer.heartfulness.org>
2023-08-17 15:00:39 +0530

    to do app first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage';
+import TaskListPage from './pages/TaskListPage';
+import CreateTaskPage from './pages/CreateTaskPage';
+import ViewTaskPage from './pages/ViewTaskPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TaskListPage />} />
+        <Route path='/create' element={<CreateTaskPage />} />
+        <Route path="/create/:id" element={<CreateTaskPage />} />
+        <Route path='/view' element={<ViewTaskPage />} />
+        <Route path="/view/:id" element={<ViewTaskPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
